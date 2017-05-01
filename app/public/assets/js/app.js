fb79be30bad026bfd715d69f23cddd7f29d94691
Max Gardner <max.c.gardner@gmail.com>
2017-04-30 22:16:01 -0500

    Add client-side Javascript to handle incoming form submissions and comparing of survey responses

--- app/public/assets/js/app.js
+++ app/public/assets/js/app.js
@@ -1,8 +1,56 @@
+function compareFriends(newSurvey, currSurveys) {
+  let compatibility;
+  for (i in currSurveys) {
+    for (j in currSurveys[i]) {
+      console.log("CURRENT SURVEYS!!! " + currSurveys[i][j]);
+      compatibility += Math.abs(parseInt(currSurveys[i][j]) - parseInt(newSurvey[j]));
+    }
+  }
+  console.log(compatibility);
+  return compatibility;
+}
+
+function getRequest(newSurvey) {
+  $.get("/api/friends", (response) => {
+    let surveys = [];
+    for (i in response) {
+      surveys.push(response[i].survey);
+    }
+    compareFriends(newSurvey, surveys);
+  });
+}
+
+// Event listener for survey completion
 $("#complete-survey").on("click", (event) => {
   event.preventDefault();
 
   let name = $("#name").val().trim();
   let photo = $("#photo").val().trim();
+  let survey = [
+    parseInt($("#q1 option:selected").val()),
+    parseInt($("#q2 option:selected").val()),
+    parseInt($("#q3 option:selected").val()),
+    parseInt($("#q4 option:selected").val()),
+    parseInt($("#q5 option:selected").val()),
+    parseInt($("#q6 option:selected").val()),
+    parseInt($("#q7 option:selected").val()),
+    parseInt($("#q8 option:selected").val()),
+    parseInt($("#q9 option:selected").val()),
+    parseInt($("#q10 option:selected").val())
+  ];
 
-  
-})
+  // Store survey info as an object
+  let newFriend = {
+    name: name,
+    photo: photo,
+    survey: survey
+  };
+
+  getRequest(survey);
+
+  // Send post request to server with new object
+  $.post("/api/friends", newFriend)
+    .done((data) => {
+      console.log(data);
+    });
+});
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -4,33 +4,19 @@ let fs = require("fs");
 // Build routes to serve HTML pages to user based on their URL path
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
-    fs.readFile('/../data/friends.txt', (err, data) => {
+    fs.readFile(__dirname + '/../data/friends.txt', (err, data) => {
       if (err) throw err;
+      console.log(data);
       let friendArray = [JSON.parse(data)];
       res.send(friendArray);
     });
   });
 
   app.post("/api/friends", function(req, res) {
-    let newFriend = {
-      name: req.body.name,
-      photo: req.body.photo,
-      survey: [
-        req.body.q1,
-        req.body.q2,
-        req.body.q3,
-        req.body.q4,
-        req.body.q5,
-        req.body.q6,
-        req.body.q7,
-        req.body.q8,
-        req.body.q9,
-        req.body.q10
-      ]
-    }
-    fs.appendFile("/../data/friends.txt", ", " + newFriend, (err) => {
+    console.log(req.body);
+    fs.appendFile(__dirname + "/../data/friends.txt", ", " + JSON.stringify(req.body.newFriend), (err) => {
       if (err) throw err;
-      console.log("Friend successfully added! Look at you, being all social.");
+
     });
   });
 }
